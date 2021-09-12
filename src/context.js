76b54e054c4b155370a482db591020fd8f14a85e
Ilya Mur <ilyamur@hotmail.com>
2021-09-12 17:21:34 +0300

    init commit

---
+++ src/context.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+};
+
+const API_ENDPOINT = "https://opentdb.com/api.php?";
+
+const url =
+  "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
