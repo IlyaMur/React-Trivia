b85f1c933ad471e85cecdc7deaef3b8d5ebc546e
Ilya Mur <ilyamur@hotmail.com>
2021-09-13 06:55:53 +0300

    Updates

---
+++ precache-manifest.73d294d1fa74109781943e89d3734e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961468ab010c2d76afa3ac0d5f95c287",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "0e3096eee276a614d4bb",
+    "url": "/react-trivia/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "11fd51e5730b832a9754",
+    "url": "/react-trivia/static/js/2.20f6f6e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-trivia/static/js/2.20f6f6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3096eee276a614d4bb",
+    "url": "/react-trivia/static/js/main.de5787b8.chunk.js"
+  },
+  {
+    "revision": "c7f8aa19b08ae7038721",
+    "url": "/react-trivia/static/js/runtime-main.a1668dfd.js"
+  }
+]);
